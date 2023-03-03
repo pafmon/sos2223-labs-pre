4ea3680460cb17abdf5f94c18feee1917a269c2a
@pafmon <pafmon@gmail.com>
2023-03-03 10:04:41 +0100

    version endpoint

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ app.get("/faces", (request,response) => {
     response.send(cool());
     console.log("New /faces request");
 });
+
 app.get("/resource", (request,response) => {
     response.json({id:"myId"});
     console.log("New /resource request");
 });
 
+app.get("/version", (request,response) => {
+    response.json({version:"1.0"});
+    console.log("New /version request");
+});
 
 app.listen(port,()=>{
     console.log(`Server ready in port ${port}`);
